e0144ee469ef3b9b9871d14e4bc9a1e05a6ff011
Nitzan Mordechai <nmordech@redhat.com>
2025-12-11 06:32:31 +0000

    mgr/prometheus: metrics header for standby module
    PR #65245 drop the header set for standby module,
    we should still have it.
    
    Fixes: https://tracker.ceph.com/issues/74149
    Signed-off-by: Nitzan Mordechai <nmordech@ibm.com>

--- src/pybind/mgr/prometheus/module.py
+++ src/pybind/mgr/prometheus/module.py
@@ -2138,6 +2138,7 @@ class Module(MgrModule, OrchestratorClientMixin):
                 # Lock the function execution
                 assert isinstance(_global_instance, Module)
                 with _global_instance.collect_lock:
+                    cherrypy.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
                     return self._metrics(_global_instance)
 
             @staticmethod
@@ -2354,6 +2355,7 @@ class StandbyModule(MgrStandbyModule):
 
             @cherrypy.expose
             def metrics(self) -> str:
+                cherrypy.response.headers['Content-Type'] = 'text/plain; charset=utf-8'
                 return ''
 
         config = {
